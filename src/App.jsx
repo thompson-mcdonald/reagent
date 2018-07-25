163a8e47093fa3133b32de4ec8f1a262beab77fc
Josh McMillan <josh@joshmcmillan.co.uk>
2018-07-25 07:45:55 +0100

    Add Wrapper component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Wrapper from "./components/Wrapper/Wrapper";
 import Start from "./pages/Start";
 import Form from "./pages/Form";
 import Results from "./pages/Results";
@@ -11,16 +12,13 @@ export default () => (
     <Fragment>
       <Header />
 
-      <div className="container bg-white bo-top pad-v pad-h">
-        <div className="d-b">
-          <Switch>
-            <Route exact path="/" component={Start} />
-            <Route path="/form" component={Form} />
-            <Route path="/results/d/:drugs/r/:reagents" component={Results} />
-          </Switch>
-          <div className="clear" />
-        </div>
-      </div>
+      <Wrapper>
+        <Switch>
+          <Route exact path="/" component={Start} />
+          <Route path="/form" component={Form} />
+          <Route path="/results/d/:drugs/r/:reagents" component={Results} />
+        </Switch>
+      </Wrapper>
     </Fragment>
   </Router>
 );
---
+++ src/components/Wrapper/Wrapper.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default styled.main`
+  background-color: #fff;
+  width: 100vw;
+  max-width: 850px;
+  margin: 0 auto;
+  padding: 2rem;
+  border-top: #160d2a 2px dashed;
+`;
